f0c0f34c655568af6236e1ed71115b4c653d667f
Jatin Kathuria <jatinkathuria16@gmail.com>
2024-03-30 18:15:32 +0530

    Update: Eslint and add shadcn

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'hmwv9pdqjivtem1p.public.blob.vercel-storage.com',
-          port: '',
-        },
-      ],
-    },
-  };
-   
-  module.exports = nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'e0mbbn9i0sutfxkv.public.blob.vercel-storage.com',
+        port: '',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
